b2d0bdd0c2b5d5d6e523294c9225035bf6de469f
Vinicns <vinicns91@gmail.com>
2026-06-28 00:10:02 -0300

    fix: evita erro de duplicate key no startup

--- db_setup.py
+++ db_setup.py
@@ -11,4 +11,4 @@ def init_db(app):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     with app.app_context():
-        db.create_all()
+        db.create_all(checkfirst=True)
